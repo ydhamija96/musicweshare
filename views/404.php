00962211e845ff4039478bcf470ac87684007865
Yogesh Dhamija <ydhamija96@gmail.com>
2016-01-15 19:30:09 -0500

    Added a 404 page.

--- views/404.php
+++ views/404.php
@@ -1,1 +1,20 @@
-404: Page not found!
+<?php include('includes/title.php') ?>
+<?php include('includes/sidebar.php') ?>
+
+<style>
+    #errordiv{
+        width:350px;
+        margin-top:50px;
+        text-align:center;
+        font-size:30px;
+    }
+    #errordiv > span{
+        font-weight:bold;
+    }
+</style>
+
+<div id="errordiv">
+    <span>Error 404:</span><br /> Page not found.
+</div>
+
+<?php include('includes/closing.php') ?>
