71fc939195ed9fff3ee5bca4ffa4470b6e928aee
Yogesh Dhamija <ydhamija96@gmail.com>
2016-06-18 18:24:33 -0400

    Fixed stupid merge errors

--- views/message.php
+++ views/message.php
@@ -8,12 +8,6 @@
 		</a>
 	<?php endif; ?>	-->
 	<?php echo $message; ?>
-<<<<<<< HEAD
-=======
-	<!--<span class="glyphicon glyphicon-heart heart-icon" aria-hidden="true">
-		<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-	</span>-->
->>>>>>> e7cff5248d50f22c147986dffeefcbc8ead9017a
 </div>
 
 <?php include('includes/closing.php') ?>
