da83fe3db475f573a93243feca70cbc4d67b0d14
Yogesh Dhamija <ydhamija96@gmail.com>
2016-06-18 18:19:38 -0400

    Updated message and made the damn thing less cringy

--- views/message.php
+++ views/message.php
@@ -2,15 +2,12 @@
 <?php include('includes/sidebar.php') ?>
 
 <div style="width:350px;">
-	<?php if($gifts): ?>
+	<!-- <?php if($gifts): ?>
 		<a href="<?php echo Config::get('base'); ?>gift/">
 			<span class="glyphicon glyphicon-gift option giftbox" aria-hidden="true"></span>
 		</a>
-	<?php endif; ?>
+	<?php endif; ?>	-->
 	<?php echo $message; ?>
-	<span class="glyphicon glyphicon-heart heart-icon" aria-hidden="true">
-		<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-	</span>
 </div>
 
 <?php include('includes/closing.php') ?>
