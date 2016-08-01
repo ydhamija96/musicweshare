00c557efac4a4955deb671a4b3494e519d4cef94
Yogesh Dhamija <ydhamija96@gmail.com>
2016-08-01 06:50:56 -0400

    Forgot to remove the gift option for single songs.

--- views/single.php
+++ views/single.php
@@ -10,9 +10,9 @@
                 ?>
             </div>
             <div class="extra">
-                <a target="invisibleFrame" rel="nofollow" href="<?php echo Config::get('base')."gift/".$idString."/"; ?>">
+                <!-- <a target="invisibleFrame" rel="nofollow" href="<?php echo Config::get('base')."gift/".$idString."/"; ?>">
                     <span class="glyphicon glyphicon-gift option" aria-hidden="true"></span>
-                </a>
+                </a> -->
                 <iframe name="invisibleFrame" id="invisibleFrame"></iframe>
             </div>
         </div>
